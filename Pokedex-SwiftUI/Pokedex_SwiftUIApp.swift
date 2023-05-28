5ad605eb86ae8bb113f687435158de994425c129
Akın Aksoy <akinaksoytr@gmail.com>
2023-05-28 20:50:36 +0300

    Initial Commit

---
+++ Pokedex-SwiftUI/Pokedex_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_SwiftUIApp.swift
+//  Pokedex-SwiftUI
+//
+//  Created by Akın Aksoy on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
